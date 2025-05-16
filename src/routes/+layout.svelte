9988481c2c069f7bb602876fade2a73b02b14b1c
Kit Allen <kitallen23@gmail.com>
2025-05-17 01:05:35 +1000

    Feat: finished configuring main libraries

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
-	import '../app.css';
+    import "../app.css";
+    import Timer from "virtual:icons/material-symbols-light/timer";
 
-	let { children } = $props();
+    let { children } = $props();
 </script>
 
-{@render children()}
+<header class="bg-base-200 fixed top-0 right-0 w-full shadow-md">
+    <div class="mx-auto flex h-10 w-full max-w-4xl items-center justify-between px-4">
+        <a href="/" class="app-title flex items-center gap-1"><Timer />stopwatch.</a>
+    </div>
+</header>
+
+<main class="pt-10">
+    {@render children()}
+</main>
+
+<style>
+    header {
+        z-index: 1;
+    }
+
+    .app-title {
+        font-family: monospace;
+        font-size: 1rem;
+    }
+</style>
