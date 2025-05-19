4b992821b897ea1d034ab6217f55aef33bd3f902
Kit Allen <kitallen23@gmail.com>
2025-05-20 01:37:13 +1000

    Feat: added zen mode

--- src/lib/config/constants.ts
+++ src/lib/config/constants.ts
@@ -1,3 +1,5 @@
+const LOCAL_STORAGE_PREFIX = "stopwatch";
 export const LOCAL_STORAGE_KEYS = {
-    theme: "stopwatch-theme",
+    theme: `${LOCAL_STORAGE_PREFIX}-theme`,
+    zenMode: `${LOCAL_STORAGE_PREFIX}-zen-mode`,
 };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,35 @@
 <script lang="ts">
     import "../app.css";
     import Timer from "virtual:icons/material-symbols/timer";
+    import Flower from "virtual:icons/material-symbols/local-florist";
+    import FlowerOutline from "virtual:icons/material-symbols/local-florist-outline";
     import ThemeDropdown from "./ThemeDropdown.svelte";
+    import { zenMode } from "$lib/stores/zenMode";
 
     let { children } = $props();
+
+    function toggleZenMode() {
+        zenMode.update((currentValue) => !currentValue);
+    }
 </script>
 
 <header class="bg-base-200 fixed top-0 right-0 w-full">
-    <div class="mx-auto flex h-10 w-full max-w-4xl items-center justify-between px-4">
+    <div class="mx-auto flex h-10 w-full max-w-4xl items-center justify-between gap-2 px-4">
         <a href="/" class="app-title flex items-center gap-1 text-lg"><Timer />stopwatch.</a>
-        <ThemeDropdown />
+        <div class="flex items-center justify-end gap-2">
+            {#if $zenMode}
+                <button class="btn btn-ghost btn-sm text-base-content/50" onclick={toggleZenMode}
+                    ><span class="hidden sm:inline">zen mode: on </span><Flower
+                        class="text-primary"
+                    /></button
+                >
+            {:else}
+                <button class="btn btn-ghost btn-sm text-base-content/50" onclick={toggleZenMode}
+                    ><FlowerOutline /></button
+                >
+            {/if}
+            <ThemeDropdown />
+        </div>
     </div>
 </header>
 
