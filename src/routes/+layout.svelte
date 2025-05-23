0ce0fe8969a1d97cc89d8c9ec20f24c79d03c2c0
Kit Allen <kitallen23@gmail.com>
2025-05-23 16:17:18 +1000

    Feat: reduced max width of header & footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,7 +15,7 @@
 </script>
 
 <header class="bg-base-200 fixed top-0 right-0 w-full">
-    <div class="mx-auto flex h-10 w-full max-w-4xl items-center justify-between gap-2 px-4">
+    <div class="mx-auto flex h-10 w-full max-w-2xl items-center justify-between gap-2 px-4">
         <a href="/" class="app-title flex items-center gap-1 text-lg"><Timer />stopwatch.</a>
         <div class="flex items-center justify-end gap-2">
             {#if $zenMode}
@@ -40,7 +40,7 @@
 
 <footer class="footer bg-base-200 text-base-content/50 place-items-end px-2 py-1">
     <aside class="w-full">
-        <div class="mx-auto flex w-full max-w-4xl items-center justify-end gap-10 text-xs">
+        <div class="mx-auto flex w-full max-w-2xl items-center justify-end gap-10 text-xs">
             <span>app v{APP_VERSION}</span>
             <div class="flex items-center">
                 <span
